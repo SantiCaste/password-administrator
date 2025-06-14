40f7c08db68bd4b1a6b49d804d5119e152185174
SantiCastellani <45802472+SantiCaste@users.noreply.github.com>
2025-06-14 20:51:26 -0300

    feat: update menu options and integrate change_user_name and delete_user functionalities

--- password-administrator/administrator/main.py
+++ password-administrator/administrator/main.py
@@ -1,4 +1,4 @@
-from admin import change_password, show_registers, handle_registration
+from admin import handle_registration, change_password, change_user_name, delete_user, show_registers
 
 
 def print_banner():
@@ -7,24 +7,32 @@ def print_banner():
     print("="*35)
     print("1. Add user")
     print("2. Change password")
-    print("3. Show registered users")
-    print("4. Exit")
+    print("3. Change user name")
+    print("4. Delete user")
+    print("5. Show registered users")
+    print("6. Exit")
     print("="*35)
 
 if __name__ == "__main__":
     while True:
         print_banner()
-        choice = input("Choose an option (1-4): ")
+        choice = int(input("Choose an option (1-5): "))
         print()
-        if choice == "1":
+        if choice == 1:
             handle_registration()
-        elif choice == "2":
+        elif choice == 2:
             name = input("Insert the name: ")
             change_password(name)
-        elif choice == "3":
+        elif choice == 3:
+            cur_name = input("Insert the current name: ")
+            change_user_name(cur_name)
+        elif choice == 4:
+            name = input("Insert the name to delete: ")
+            delete_user(name)
+        elif choice == 5:
             show_registers()
-        elif choice == "4":
+        elif choice == 6:
             print("Exiting Password Administrator. Goodbye!\n")
             break
         else:
-            print("Invalid option, please choose a number between 1 and 4.")
+            print("Invalid option, please choose a number between 1 and 5.")
