1ef1fb00a63ec79953599d75cfa3e45ca1c992be
SantiCastellani <45802472+SantiCaste@users.noreply.github.com>
2025-06-14 19:40:29 -0300

    feat: initial implementation of add_reg, change_pwd and show_reg functions

---
+++ password-administrator/administrator/admin.py
@@ -0,0 +1,33 @@
+
+registers: dict
+
+def add_reg(name: str, pwd: str):
+    if registers[name] != "":
+        print("name already taken")
+        return
+
+    registers[name] = pwd
+
+def change_pwd(name: str):
+    # TODO: validar input (vacío por ej)
+    old_pwd = input("insert the previous password: ")
+
+    if registers[name] != old_pwd:
+        print("incorrect password")
+        return
+
+    # TODO: validar input (vacío por ej)
+    new_pwd = input("insert the new password: ")
+    registers[name] = new_pwd
+
+    print("password changed successfully")
+
+def show_reg():
+    print("Registered users:")
+    for name, pwd in registers.items():
+        if pwd != "":
+            print(f"{name}: {pwd}")
+        else:
+            print(f"{name}: not registered")
+    print()
+
