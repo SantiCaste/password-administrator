4d70924bf3ce26c89f72621af57d5b215f9cfc44
SantiCastellani <45802472+SantiCaste@users.noreply.github.com>
2025-06-14 19:42:01 -0300

    feat: added main file with the options to choose from

---
+++ password-administrator/administrator/main.py
@@ -0,0 +1,20 @@
+from admin import registers, add_reg, change_pwd, show_reg, handle_registration
+
+
+if __name__ == "__main__":
+    while True:
+        choice = input("1. Add user\n2. Change password\n3. Show registered users\n4. Exit\nChoose an option: ")
+        if choice == "1":
+            handle_registration()
+        elif choice == "2":
+            name = input("Insert the name: ")
+            if name not in registers:
+                print("User not found")
+                continue
+            change_pwd(name)
+        elif choice == "3":
+            show_reg()
+        elif choice == "4":
+            break
+        else:
+            print("Invalid option, please try again.")
